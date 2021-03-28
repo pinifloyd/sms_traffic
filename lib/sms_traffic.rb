9c7c9e7db607e6bf16ba541e7bde617d3a0aeecd
Sergey Hanchar <hanchar.sergey@gmail.com>
2021-03-28 11:19:33 +0300

    INIT

---
+++ lib/sms_traffic.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'sms_traffic/version'
+
+module SmsTraffic
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sms_traffic/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SmsTraffic
+  VERSION = '0.1.0'
+end
